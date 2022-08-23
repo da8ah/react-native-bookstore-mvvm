b830db8b83309202234644215617a5b7c9d2e511
Tiber <danilo.ochoa.hidalgo@outlook.com>
2022-08-22 21:00:07 -0500

    usecase: rectifying display books usecase

--- src/core/usecases/DisplayBooks.ts
+++ src/core/usecases/DisplayBooks.ts
@@ -2,7 +2,7 @@ import { Book } from "../entities/Book";
 import { IRepository } from "../ports/IRepository";
 
 export class DisplayBooks {
-    public queryAllBooks(iRepository: IRepository): Promise<Book[]> {
+    public queryAllBooks(iRepository: IRepository): Promise<Book[] | null> {
         return iRepository.queryAllBooks();
     }
 }
